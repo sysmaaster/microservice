d196a070729d65bd69c075b7a1efdfd0634611f0
sysMaaster <sysMaaster@gmail.com>
2024-04-17 01:50:47 +0300

    0.0.0.3/1

--- public/assets/js/front.js
+++ public/assets/js/front.js
@@ -1,6 +1,8 @@
-const Front = {
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+createApp({
   data() {
     return {
+      categories: [],
       wallets: [],
       count: 0,
     };
@@ -30,7 +32,7 @@ const Front = {
   async mounted() {
     const res = await fetch("/wallet");
     this.wallets = await res.json();
+    const categories = await fetch("/categories");
+    this.categories = await categories.json();
   },
-};
-
-Vue.createApp(Front).mount("#front");
+}).mount("#app");
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import app from "./app";
 import logger from "./services/logger";
 import { startMetricsServer } from "./services/metrics";
 import connect from "./services/connect";
+import { createServer } from "http";
+import socketIO from "./utils/socket";
 
 function startServer() {
-  const port = 1242;
-  app.listen(port, async () => {
+  const server = createServer(app);
+
+  //socketIO(server)
+
+  const port: string | number = process.env.PORT || 1242;
+  server.listen(port, async () => {
     logger.info(`Example App listening at http://localhost:${port}`);
     startMetricsServer();
     await connect();
