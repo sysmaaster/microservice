439e5e8401374b7bfb975bb49010027d4cbafe84
sysMaaster <sysMaaster@gmail.com>
2024-04-18 17:45:18 +0300

    0.0.0.3/5

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import responseTime from "response-time";
 import swaggerDocs from "./services/swagger";
 import DevsRouter from "./router/devs.routes";
+import UploadRouter from "./router/upload.routes";
 import WalletRouter from "./router/wallet.routes";
 import CategoriesRouter from "./router/categories.routes";
 import errorMiddleware from "./middleware/error.middleware";
@@ -12,21 +13,11 @@ import errorLoggerHandler from "./handler/errorLogger.handler";
 import { restResponseTimeHistogram } from "./services/metrics";
 import invalidPathHandler from "./handler/invalidPath.handler";
 import BasicAuthMiddleware from "./middleware/basicAuth.middleware";
-import UploadRouter from "./router/upload.routes";
 
 const port: string | number = process.env.PORT || 1242;
 
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json()); // parse form data client
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const _dirname = path.resolve(path.resolve(), "./public");
-app.use(express.static(path.resolve(path.resolve(), "./public")));
-
-app.set("view engine", "ejs"); // configure template engine
-app.set("views", path.resolve(_dirname, "views")); // set express to look in this folder to render our view
 app.use(
   responseTime(
     (request: express.Request, response: express.Response, time: number) => {
@@ -43,6 +34,23 @@ app.use(
     }
   )
 );
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE,HEAD,PATCH",
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.use(express.static(path.resolve(path.resolve(), "public")));
+
+app.set("view engine", "ejs"); // configure template engine
+app.set("views", path.resolve(path.resolve(), "views")); // set express to look in this folder to render our view
+
+app.use(bodyParser.json()); // parse form data client
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/healthcheck", (req, res) => res.sendStatus(200));
 app.get("/", (req, res) => {
--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,23 +1,33 @@
-import logger from "pino";
 import dayjs from "dayjs";
 import path from "path";
+import pino from "pino";
 const _dirname = path.resolve(path.resolve(), "./");
 
-const log = logger({
-  transport: {
-    targets: [
-      {
-        target: "pino/file",
-        options: { hideColor: true, destination: `${_dirname}/app.log` },
-      },
-      {
-        target: "pino-pretty",
-        options: { colorize: true },
-      },
-    ],
-  },
-  base: {},
-  timestamp: () => `,"time":"${dayjs().format()}"`,
+const transport = pino.transport({
+  targets: [
+    {
+      target: "pino/file",
+      options: { hideColor: true, destination: `${_dirname}/app.log` },
+    },
+    {
+      target: "pino-pretty",
+      options: { colorize: true },
+    },
+  ],
 });
+
+const log = pino(
+  {
+    base: {},
+    level: process.env.LOG_LEVEL || "info", //
+    timestamp: () => `,"time":"${dayjs().format()}"`, //2024-04-18T16:10:18+03:00
+    redact: {
+      paths: ["user.name", "user.address", "user.passport", "user.phone"],
+      censor: "[ТАЙНА]",
+      //remove: true, //полностью удалить эти поля
+    },
+  },
+  transport
+);
 
 export default log;
